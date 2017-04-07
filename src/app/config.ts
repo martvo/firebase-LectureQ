9bce16592fa224f2159c6746d96ebd89cc56a23d
Ole <ole.barsch@gmail.com>
2017-04-07 10:21:36 +0200

    delete config.ts
    Denne skal ikke være på github

--- src/app/config.ts
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDWUtLOkA-E2uKUIjzjL_Rxlxt4uMcSwNM",
-  authDomain: "fir-lectureq.firebaseapp.com",
-  databaseURL: "https://fir-lectureq.firebaseio.com",
-  storageBucket: "fir-lectureq.appspot.com",
-  messagingSenderId: "937804058730"
-};
